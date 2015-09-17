45b30abb9cfbb9d29b70ddd1c8f5ff0be5ad59c2
Mark Lemming <lemming@quirkyqatz.com>
2015-09-17 10:25:19 -0600

    changes

--- var/upgrades/3.0912.sql
+++ var/upgrades/3.0912.sql
@@ -1,2 +1,3 @@
 alter table body add column notes text;
 alter table propositions drop column votes_yes, drop column votes_no, drop column votes_needed;
+alter table ships add column views mediumblob;
